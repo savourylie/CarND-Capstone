3d085c5316f204714ac8333ff804d14744c7c8b2
Ubuntu <ubuntu@ip-10-191-9-96.us-west-1.compute.internal>
2018-06-28 22:20:51 +0000

    Relativize path

--- traffic-light-mobilenetv2-classifier/test_classify.sh
+++ traffic-light-mobilenetv2-classifier/test_classify.sh
@@ -2,7 +2,7 @@
 python -c "import tensorflow as tf; print('TensorFlow version', tf.__version__)"
 
 echo 'Classifying real_pic/0/left1575.jpg'
-python classify_traffic_light_image.py --image /home/ubuntu/CarND-Capstone/traffic-light-mobilenetv2-classifier/left1575.jpg
+python classify_traffic_light_image.py --image ./left1575.jpg
 
 echo 'Classifying sim_pic/0/sim_img_04238.jpg'
 python classify_traffic_light_image.py --image ./sim_img_04238.jpg
